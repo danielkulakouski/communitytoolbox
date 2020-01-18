85591959ec7bec6602268fff0de298ffd8ad5439
Daniel Kulakouski <dan.kulakouski@gmail.com>
2020-01-18 17:10:33 -0500

    forms

---
+++ login.php
@@ -0,0 +1,17 @@
+<div id="formwbuttons">
+<div id="login" class="forms">
+	<form class="loginform" action="login.php" method="POST">
+		<label>Email</label>
+		<br/>
+		<input type="text" name="l_email" placeholder=""  id="inputfield">
+		<!-- <?php echo $errors['email']; ?> -->
+		<br/>
+		<!-- Password -->
+		<label>Password</label>
+		<br/>
+		<input type="password" name="l_password1" id="inputfield">
+		<br/>
+		<input type="submit" name="login" value="Login" id="submitbutton">
+	</form>
+</div>
+</div>
