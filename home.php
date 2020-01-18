015b2e968f78d5b8e7e266ff9cafa538cf6fc3e3
Tomer Zibman <tomerzibman@v1040-wn-rt-c-15-179.campus-dynamic.uwaterloo.ca>
2020-01-18 15:32:23 -0500

    changed extnesion to php

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="CSS/style_index.css"/>
+</head>
+<body>
+    <div class="content">
+        <div class="wrap">
+            <h1 class="bigtext">Tools</h1>
+            	<input type = "text" placeholder = "search by tool name" />
+            <aside>
+                <ul>
+                    <li><a href="#">Logout</a></li>
+                    <li><a href="#">Account Settings</a></li>
+                    <li><a href="home.html">Home</a></li>
+                </ul>
+            </aside>
+            <div class="content">
+    
+            </div>
+        </div>		
+</body>
