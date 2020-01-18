138f44dd178bcd9cba1a60863e11455466a48778
Tomer Zibman <tomerzibman@v1040-wn-rt-c-15-179.campus-dynamic.uwaterloo.ca>
2020-01-18 16:44:37 -0500

    Stuff lmao

--- home.php
+++ home.php
@@ -4,5 +4,9 @@
 
 <!DOCTYPE html>
     <body>
+        <div class="toolscreen">
+            <h1>Tools</h1>
+            <input type="text" placeholder="Seach for tools..."/>
+        </div>
     </body>
 </html>
