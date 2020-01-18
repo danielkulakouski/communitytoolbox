5527c410b2d02942656f23446bcb2965532275aa
Daniel Kulakouski <dan.kulakouski@gmail.com>
2020-01-18 16:20:58 -0500

    added kul header

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS/style_index.css"/>
+    <link rel="icon" href="images/icon.png">
+    <title>Community Toolbox</title>
+    <div id="header">
+        <div id="logo" style="float: left">
+        <a href = "#"><img src = "images/logo.png" alt = "Logo" height = "90" width = "230"></a>
+    </div>
+    <div id="headerLinks" style="float: right">
+        <a href="home.php">HOME</a>
+        <a href="#">SETTINGS</a>
+        <a href="#">LOGOUT</a>
+    </div>
+</div>
+</head>
+<body>
