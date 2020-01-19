aad1cb83a26a99c0d655038d61debeab4541ad81
Daniel Kulakouski <dan.kulakouski@gmail.com>
2020-01-19 13:23:47 -0500

    fefs

--- home.php
+++ home.php
@@ -1,9 +1,28 @@
+<!DOCTYPE html>
+<html>
 <?php
     include('homeHeader.php');
 ?>
 
-<!DOCTYPE html>
     <body>
-
+      daldsklfjha.dskjfhadsfkjhalkh
+      <?php 
+            session_start();
+            include_once 'dbh.inc.php';
+            $query = "SELECT tool_name, price FROM Tools;";
+            $results = mysqli_query($conn, $query);
+            // $tool_rows = mysqli_fetch_array($results);
+            echo "<table border='1'>"
+            while($row = mysqli_fetch_assoc($results)) {
+              echo "<tr>";
+              foreach($row as $field => $value) {
+                echo "<td>" . $value . "</td>";
+              }
+              echo "</tr>";
+                // echo "Tool Name: ".($tool_rows['tool_name']);
+              // }
+            } 
+            echo "</table>"?>
     </body>
+
 </html>
--- homeHeader.php
+++ homeHeader.php
@@ -8,13 +8,15 @@
     <title>Community Toolbox</title>
     <div id="header">
         <div id="logo" style="float: left">
-        <a href = "home.php">
-        <img src = "images/logo.png" alt="logo" height = "90" width = "230"></a>
-    </div>
-    <div id="headerLinks" style="float: right">
-        <a href="home.php">HOME</a>
-        <a href="add_tool.php">ADD TOOL</a>
-        <a href="#">LOGOUT</a>
-    </div>
-</div>
-</head>
+            <a href = "home.php">
+                <img src = "images/logo.png" alt="logo" height = "90" width = "230"></a>
+            </div>
+            <div id="headerLinks" style="float: right">
+                <a href="home.php">HOME</a>
+                <a href="add_tool.php">ADD TOOL</a>
+                <a href="#">LOGOUT</a>
+            </div>
+            
+
+        </div>
+    </head>
