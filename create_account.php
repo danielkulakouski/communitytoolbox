05be48c89830f98d407a3499215da47f5b2669ee
Daniel Kulakouski <dan.kulakouski@gmail.com>
2020-01-19 12:56:24 -0500

    fejskfn

--- create_account.php
+++ create_account.php
@@ -4,60 +4,57 @@
 
     <!DOCTYPE html>
     <body>
-
-        <!-- <div id="formButtons"> -->
-            <!-- <button class="formlink" action="login.php">Login</button>
-                <button class="formlink" action="register.php" id="badRegister">Create an Account</button> -->
-
-                <a href="login.php" class="formlink">Login</a>
-                <a href="create_account.php" class="formlink">Register</a>
-	<div id="formwbuttons">
-		<h1 class="bigtext">Creat a Community Toolbox Account</h1>
-		<!-- <div class="content"> -->
-			<form action="includes/creare_account.inc.php" method="POST">
-				<p class="smalltext">First Name</p>
-				<input type="text" name="first"/>
-				<p class="smalltext">Last Name</p>
-				<input type="text" name="last"/>
-				<p class="smalltext">Email</p>
-				<input type="text" name="email"/>
-				<p class="smalltext">Password</p>
-				<input type="password" name="password"/>
-				<p class="smalltext">Re-enter Password</p>
-				<input type="password" name="pwdRe"/>
-				<br/>
-				<button type="submit" name="submit">Create Account</button>
-			</form>
-			<?php
-				$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-				if(strpos($fullUrl, "signup=empty") == true) {
-					echo "<p class='error'>You did not fill out all fields</p>";
-				}
-				if(strpos($fullUrl, "signup=invalidemail") == true) {
-					echo "<p class='error'>You entered an invalid email</p>";
-				}
-				if(strpos($fullUrl, "signup=emailexists") == true) {
-					echo "<p class='error'>The email you entered already exists</p>";
-				}
-				if(strpos($fullUrl, "signup=char") == true) {
-					echo "<p class='error'>You are using invalid charachters</p>";
-				}
-				if(strpos($fullUrl, "signup=pwd") == true) {
-					echo "<p class='error'>Your password is too short</p>";
-				}
-				if(strpos($fullUrl, "signup=error") == true) {
-					echo "<p class='error'>Please fill out the form</p>";
-				}
-				if(strpos($fullUrl, "signup=notmatch") == true) {
-					echo "<p class='error'>The passwords you entered did not match</p>";
-				}
-				if(strpos($fullUrl, "signup=success") == true) {
-					echo "<p class='success'>Your account was successfully created!</p>";
-					header("Location: home.php");
-				}
-			?>
-			<p id=x>Already have an account? Click <a class="link" href="login.php">here</a> to login.</p>
-		<!-- </div> -->
-	</div>
-</body>
-</html>
+    	<!-- <div class="links"> -->
+    		<div class="formButtons">
+    			<a href="login.php" class="formlink">Login</a>
+    			<a href="create_account.php" class="formlink">Register</a>
+    		<!-- </div> -->
+    	</div>
+    	<div id="formwbuttons">
+    		<div id="register">
+    			<form id="register" action="includes/creare_account.inc.php" method="POST">
+    				<p class="smalltext">First Name</p>
+    				<input type="text" name="first"/>
+    				<p class="smalltext">Last Name</p>
+    				<input type="text" name="last"/>
+    				<p class="smalltext">Email</p>
+    				<input type="text" name="email"/>
+    				<p class="smalltext">Password</p>
+    				<input type="password" name="password"/>
+    				<p class="smalltext">Re-enter Password</p>
+    				<input type="password" name="pwdRe"/>
+    				<br/>
+    				<button type="submit" name="submit">Create Account</button>
+    			</form>
+    			<?php
+    			$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    			if(strpos($fullUrl, "signup=empty") == true) {
+    				echo "<p class='error'>You did not fill out all fields</p>";
+    			}
+    			if(strpos($fullUrl, "signup=invalidemail") == true) {
+    				echo "<p class='error'>You entered an invalid email</p>";
+    			}
+    			if(strpos($fullUrl, "signup=emailexists") == true) {
+    				echo "<p class='error'>The email you entered already exists</p>";
+    			}
+    			if(strpos($fullUrl, "signup=char") == true) {
+    				echo "<p class='error'>You are using invalid charachters</p>";
+    			}
+    			if(strpos($fullUrl, "signup=pwd") == true) {
+    				echo "<p class='error'>Your password is too short</p>";
+    			}
+    			if(strpos($fullUrl, "signup=error") == true) {
+    				echo "<p class='error'>Please fill out the form</p>";
+    			}
+    			if(strpos($fullUrl, "signup=notmatch") == true) {
+    				echo "<p class='error'>The passwords you entered did not match</p>";
+    			}
+    			if(strpos($fullUrl, "signup=success") == true) {
+    				echo "<p class='success'>Your account was successfully created!</p>";
+    				header("Location: home.php");
+    			}
+    			?>
+    		</div>
+    	</div>
+    </body>
+    </html>
--- login.php
+++ login.php
@@ -1,69 +1,37 @@
-<!-- <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title>Login</title>
-    </head>
-    <body>
-        <div class="wrap">
-            <h1>Login here</h1>
-            <div class="content">
-                <form action="#" method="POST">
-                    <p>Email<p>
-                    <input type="text" name="email"/>
-                    <p>Password</p>
-                    <input type="password" name="password"/>
-                    <br/>
-                    <button type="submit" name="submit">Login</button>
-                </form>
-                <?php
-				    $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-				    if(strpos($fullUrl, "login=failed") == true) {
-					    echo "<p class='error'>The email and password did not macth</p>";
-				    }
-			    ?>
-                <p id=x>Don't have an account? Click <a href="create_account.php">here</a> to sign up.</p>
-            </div>
+<?php
+include('header.php');
+?>
+
+<!DOCTYPE html>
+<body>
+    <!-- <div class="links"> -->
+        <div class="formButtons">
+            <a href="login.php" class="formlink">Login</a>
+            <a href="create_account.php" class="formlink">Register</a>
         </div>
-    </body>
-    </html> -->
+    <!-- </div> -->
 
-    <?php
-    include('header.php');
-    ?>
-
-    <!DOCTYPE html>
-    <body>
-
-        <!-- <div id="formButtons"> -->
-            <!-- <button class="formlink" action="login.php">Login</button>
-                <button class="formlink" action="register.php" id="badRegister">Create an Account</button> -->
-
-                <a href="login.php" class="formlink">Login</a>
-                <a href="create_account.php" class="formlink">Register</a>
-                <!-- </div> -->
-
-                <div id="formwbuttons">
-                    <div id="login" >
-                     <form class="loginform" action="login.php" method="POST">
-                      <label>Email</label>
-                      <br/>
-                      <input type="text" name="email" placeholder=""  id="inputfield">
-                      <br/>
-                      <!-- Password -->
-                      <label>Password</label>
-                      <br/>
-                      <input type="password" name="password" id="inputfield">
-                      <br/>
-                      <input type="submit" name="submit" value="Submit" id="submitbutton">
-                  </form>
-                  <?php
-                  $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-                  if(strpos($fullUrl, "login=failed") == true) {
-                    echo "<p class='error'>The email and password did not macth</p>";
-                }
-                ?>
-            </div>
-        </div>
-    </body>
-    </html>
+    <div id="formwbuttons">
+        <div >
+            <form  id="login" class="forms" action="login.php" method="POST">
+              <label>Email</label>
+              <br/>
+              <input type="text" name="email" placeholder=""  id="inputfield">
+              <br/>
+              <!-- Password -->
+              <label>Password</label>
+              <br/>
+              <input type="password" name="password" id="inputfield">
+              <br/>
+              <input type="submit" name="submit" value="Submit" id="submitbutton">
+          </form>
+          <?php
+          $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+          if(strpos($fullUrl, "login=failed") == true) {
+            echo "<p class='error'>The email and password did not macth</p>";
+        }
+        ?>
+    </div>
+</div>
+</body>
+</html>
